ddccc2ba1070b8c15f6e2deda03f9cb625dcff3d
João Pinto <Joo Pinto@DESKTOP-NUR8BGV>
2017-06-26 19:18:01 +0100

    Inicio de pars

---
+++ WebApplication1/WebApplication1/bandas_procs1.js
@@ -0,0 +1,5 @@
+﻿function inic() {
+    document.body.innerHTML += '<h1>' + getTitulo() + '</h1>';
+    for (var i = 0; i < numApresPar() ; i++)
+        document.body.innerHTML += '<p>' + getApresPar(i) + '</p>';
+}
